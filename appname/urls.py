b77a95c3a208c21c0fd2ac9c5e3e6f05c21c6b63
Andrew Brown <brownan@gmail.com>
2016-01-04 10:09:29 -0500

    add comment about "home" view in app's urls.py

--- appname/urls.py
+++ appname/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # A view named "home" is referenced in a few places.
+    # Make sure to update the references if you change or delete this url line!
     url(r"^$", views.home, name="home"),
 ]
