f7204acb0c2e998aa80bd6d9e1dd048467c29965
Ryan P Kilby <rpkilby@ncsu.edu>
2017-04-28 17:06:10 -0400

    Add subprocess streaming output

--- project/utils/management/commands/bundle.py
+++ project/utils/management/commands/bundle.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 
 import os
+from collections import deque
 from contextlib import contextmanager
 from subprocess import check_call, Popen, PIPE
 
@@ -7,6 +9,12 @@ from django.core.management.base import BaseCommand, CommandError
 from django.utils.timezone import now
 
 DATETIME_FORMAT = '%Y%m%d_%H%M%S'
+
+# saves cursor position
+SAVE = '\x1b7'
+
+# restores cursor position and clears remainder of screen
+RESTORE = '\x1b8\x1b[0J'
 
 
 class Command(BaseCommand):
@@ -22,16 +30,40 @@ class Command(BaseCommand):
             help='Git reference to bundle, e.g. a branch or commit hash.',
         )
 
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.rows, self.cols = self.get_terminal_size()
+        self.sep = '    %s' % (u'═' * (self.cols - 8))
+
+    def get_terminal_size(self):
+        process = Popen(['stty', 'size'], stdout=PIPE)
+        out, _ = process.communicate()
+        return [int(v) for v in out.split()]
+
     @contextmanager
     def step(self, msg='', success='done', failure='fail'):
-        self.stdout.write('  - %s ' % msg, ending='')
+        self.stdout.write('  - %s %s' % (msg, SAVE))
         try:
             yield
         except Exception:
-            self.stdout.write(self.style.ERROR(failure))
+            self.stdout.write(RESTORE + self.style.ERROR(failure))
             raise
+        self.stdout.write(RESTORE + self.style.SUCCESS(success))
 
-        self.stdout.write(self.style.SUCCESS(success))
+    def stream(self, args, cwd=None):
+        """
+        Stream the output of the subprocess
+        """
+        process = Popen(args, cwd=cwd, stdout=PIPE)
+        line_buffer = deque(maxlen=self.rows - 20)
+
+        for line in iter(process.stdout.readline, ''):
+            line_buffer.append((u'    ' + line.decode('utf-8'))[:self.cols])
+            self.stdout.write(RESTORE)
+            self.stdout.write(self.sep)
+            self.stdout.write(''.join(line_buffer))
+            self.stdout.write(self.sep)
+        process.wait()
 
     def handle(self, *args, **options):
         ref = options['ref']
@@ -55,14 +87,14 @@ class Command(BaseCommand):
         # javascript build
         if os.path.exists(os.path.join(path, 'package.json')):
             with self.step('Found \'package.json\'. Building javascript...'):
-                check_call(['npm', 'install', '--only=production'], cwd=path, stdout=PIPE)
-                check_call(['npm', 'run', 'build'], cwd=path, stdout=PIPE)
+                self.stream(['npm', 'install', '--only=production'], cwd=path)
+                self.stream(['npm', 'run', 'build'], cwd=path)
         else:
             self.stdout.write('  - No \'package.json\' found. Skipping javascript build.')
 
         # Create zip archive
         with self.step('Writing bundle...'):
-            check_call(['zip', '-r', os.path.abspath(out), '.'], cwd=path, stdout=PIPE)
+            self.stream(['zip', '-r', os.path.abspath(out), '.'], cwd=path)
         self.stdout.write('')
 
         # write paths to stdout
