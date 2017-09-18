146bf8371ce94dee64cb49aa808995bd70ff8eca
Andrew Brown <ambrown4@ncsu.edu>
2017-09-18 17:17:22 -0400

    Fixed env loading in manage.py and wsgi.py
    Loading of the .env file should be conditional on the file existing so that
    environments like EB that don't use an environment file don't generate a
    warning.

--- manage.py
+++ manage.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-import os
+import os.path
 import sys
 
 import environ
 
 if __name__ == "__main__":
-    environ.Env.read_env(
-        os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
-    )
+    envfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
+    if os.path.exists(envfile):
+        environ.Env.read_env(envfile)
 
     from django.core.management import execute_from_command_line
 
--- project/wsgi.py
+++ project/wsgi.py
@@ -7,14 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os.path
 
 from django.core.wsgi import get_wsgi_application
 
 import environ
 
-environ.Env.read_env(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
-)
+envfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
+if os.path.exists(envfile):
+    environ.Env.read_env(envfile)
 
 application = get_wsgi_application()
