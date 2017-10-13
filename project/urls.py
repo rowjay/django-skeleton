bfcde27183ac65936da32edc6e7807255bd77f05
Ryan P Kilby <rpkilby@ncsu.edu>
2017-10-13 03:33:36 -0400

    Update auth urls/view, add reset views

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from project import views
 
-from appname import urls
+
+___password_reset_confirm = r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -27,10 +28,16 @@ urlpatterns = [
     # url(r'^oauth/', include("oauth.urls")),
 
     # Auth urls
-    url(r'^accounts/login$', views.LoginView.as_view(), name='login'),
-    url(r'^accounts/logout$', views.LogoutView.as_view(), name='logout'),
-    url(r'^accounts/change_password$', views.PasswordChangeView.as_view(), name='change_password'),
-    url(r'^accounts/change_password_done$', views.PasswordChangeDoneView.as_view(), name='change_password_done'),
+    url(r'^login/$',                    views.LoginView.as_view(),                  name='login'),
+    url(r'^logout/$',                   views.LogoutView.as_view(),                 name='logout'),
 
-    url(r'', include(urls)),
+    url(r'^password_change/$',          views.PasswordChangeView.as_view(),         name='password_change'),
+    url(r'^password_change/done/$',     views.PasswordChangeDoneView.as_view(),     name='password_change_done'),
+
+    url(r'^password_reset/$',           views.PasswordResetView.as_view(),          name='password_reset'),
+    url(r'^password_reset/done/$',      views.PasswordResetDoneView.as_view(),      name='password_reset_done'),
+    url(___password_reset_confirm,      views.PasswordResetConfirmView.as_view(),   name='password_reset_confirm'),
+    url(r'^reset/done/$',               views.PasswordResetCompleteView.as_view(),  name='password_reset_complete'),
+
+    url(r'^', include('appname.urls')),
 ]
--- project/views.py
+++ project/views.py
@@ -3,17 +3,33 @@ from django.contrib.auth import views as auth_views
 
 
 class LoginView(auth_views.LoginView):
-    template_name = 'login.html'
+    template_name = 'registration/auth/login.html'
+    redirect_authenticated_user = True
 
 
 class LogoutView(auth_views.LogoutView):
-    template_name = 'logout.html'
+    next_page = '/'
 
 
 class PasswordChangeView(auth_views.PasswordChangeView):
-    template_name = 'password_change.html'
-    success_url = 'change_password_done'
+    template_name = 'registration/auth/password_change_form.html'
 
 
 class PasswordChangeDoneView(auth_views.PasswordChangeDoneView):
-    template_name = 'password_change_done.html'
+    template_name = 'registration/auth/password_change_done.html'
+
+
+class PasswordResetView(auth_views.PasswordResetView):
+    template_name = 'registration/auth/password_reset_form.html'
+
+
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+    template_name = 'registration/auth/password_reset_done.html'
+
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+    template_name = 'registration/auth/password_reset_confirm.html'
+
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    template_name = 'registration/auth/password_reset_complete.html'
